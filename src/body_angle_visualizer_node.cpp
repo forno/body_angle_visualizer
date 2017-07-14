fbe6cf56ee14264a8d39588f926bd329ce9f8f70
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-07-14 20:28:44 +0900

    Update fiting for kinect coordinate system

--- src/body_angle_visualizer_node.cpp
+++ src/body_angle_visualizer_node.cpp
@@ -8,30 +8,35 @@
 #include <tf2_ros/transform_listener.h>
 
 #include <Eigen/Geometry>
+#include <Eigen/SVD>
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "body_angle_visualizer");
   ros::NodeHandle n {};
+
+  int target_number {1};
+  std::string root_name {"openni_depth_frame"};
+  {
+    ros::NodeHandle pn {"~"};
+    pn.getParam("target_number", target_number);
+    pn.getParam("root", root_name);
+  }
+
+  const auto head_name {"head_" + std::to_string(target_number)};
+  const auto torso_name {"torso_" + std::to_string(target_number)};
+
   ros::Publisher pub {n.advertise<geometry_msgs::Point>("body_direction", 1)};
   ros::Rate r {5};
   tf2_ros::Buffer tfBuffer {};
   tf2_ros::TransformListener tfListener {tfBuffer};
-  rviz_visual_tools::RvizVisualTools rvt {"openni_depth_frame", "rviz_visual_markers"};
-
-  ros::NodeHandle pn {"~"};
-  int target_number {1};
-  pn.getParam("target_number", target_number);
-  std::string root_name {"openni_depth_frame"};
-  pn.getParam("root", root_name);
-
-  const auto head_name {"head_" + std::to_string(target_number)};
-  const auto torso_name {"torso_" + std::to_string(target_number)};
+  rviz_visual_tools::RvizVisualTools rvt {root_name, "rviz_visual_markers"};
 
   while (ros::ok()) {
     try {
       const auto head_pos {tf2::transformToEigen(tfBuffer.lookupTransform(root_name, head_name, ros::Time{0}))};
       const auto torso_pos {tf2::transformToEigen(tfBuffer.lookupTransform(root_name, torso_name, ros::Time{0}))};
+      const auto torso_yrp {torso_pos.rotation().eulerAngles(1, 2, 0)};
       const auto stand_vec {head_pos.translation() - torso_pos.translation()};
       const auto stand_quaternion {Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitX(), stand_vec)};
 
@@ -42,7 +47,7 @@ int main(int argc, char** argv)
 
       Eigen::Affine3d text_pos {};
       text_pos.translation() = stand_vec;
-      rvt.publishText(text_pos, std::to_string(stand_vec.normalized()(1)), rviz_visual_tools::WHITE, rviz_visual_tools::XLARGE);
+      rvt.publishText(text_pos, std::to_string(torso_yrp(1)), rviz_visual_tools::WHITE, rviz_visual_tools::XLARGE);
 
       rvt.trigger();
     } catch (tf2::TransformException &e) {
